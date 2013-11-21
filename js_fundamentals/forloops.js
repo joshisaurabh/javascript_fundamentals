f985118996d112fff8cea1fdc884076114e2b75a
joshisaurabh <reachme15@yahoo.com>
2013-11-21 18:54:05 -0500

    for loop exercise completed

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -18,14 +18,45 @@ for ( var i= 0; i<=20; i++) {
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
 
+for ( var i= 0; i<=10; i++) {
+	console.log(i + "* 9 is "+ i*9);
+}
+
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+
+
+for ( var i= 0; i<=10; i++) {
+	for ( var j=0; j<=10; j++) {
+		console.log(i+" * "+j+"is " + i*j);
+	}
+}
 
 // EXERCISE: The Grade Assigner
 
 // Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
 
-for ( var i= 0; i<=10; i++) {
-	console.log(i + "* 9 is "+ i*9);
+function assignGrade(value) {
+	if (value>=90) {
+		console.log("For " + value +", you got an A.")
+	} else if (value<90 && value>=80) {
+		console.log("For " + value +", you got a B.")
+	} else if (value<80 && value>=70) {
+		console.log("For " + value +", you got a C.")
+	} else if (value<70 && value>=60) {
+		console.log("For " + value +", you got a D.")
+	} else if (value<60 && value>=50) {
+		console.log("For " + value +", you got a E.")
+	} else 
+		console.log("For " + value +", you got a F.")
 }
+
+
+assignGrade(90);
+assignGrade(80);
+assignGrade(70);
+assignGrade(60);
+assignGrade(50);
+assignGrade(40);
+
 
 
