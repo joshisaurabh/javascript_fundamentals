c44725d853ae68f6fad5ab3dba7956e89dd7d3fa
joshisaurabh <reachme15@yahoo.com>
2013-11-21 18:33:48 -0500

    for loop exercise completed

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -4,6 +4,16 @@
 
 // Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
 
+for ( var i= 0; i<=20; i++) {
+	if (i%2 === 0) {
+		console.log(i+"is even number");
+	} else if (i%3===0) {
+		console.log(i+"is a odd number");
+	}
+}
+
+
+
 // EXERCISE: Multiplication Tables
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
@@ -13,5 +23,9 @@
 // EXERCISE: The Grade Assigner
 
 // Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
+
+for ( var i= 0; i<=10; i++) {
+	console.log(i + "* 9 is "+ i*9);
+}
 
 
